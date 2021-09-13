43e7a43abd5fdcce70506b7b64c292aa03ebc0a0
Michał Marcin Przybysz <mio311@wp.pl>
2021-09-14 01:05:05 +0200

    Flight constrains corrections

---
+++ AirportManager/Controllers/CitiesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AirportManager.DAL;
+using AirportManager.Models;
+
+namespace AirportManager.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private AirportContext db = new AirportContext();
+
+        // GET: Cities
+        public ActionResult Index()
+        {
+            return View(db.Cities.ToList());
+        }
+
+        // GET: Cities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // GET: Cities/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cities/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cities.Add(city);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(city);
+        }
+
+        // GET: Cities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // POST: Cities/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(city).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(city);
+        }
+
+        // GET: Cities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            City city = db.Cities.Find(id);
+            db.Cities.Remove(city);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- AirportManager/Models/Flight.cs
+++ AirportManager/Models/Flight.cs
@@ -11,20 +11,18 @@ namespace AirportManager.Models
         public int ID { get; set; }
 
         [Display(Name = "Lotnisko startowe")]
-        [Required]
         public virtual Airport Start { get; set; }
 
         [Display(Name = "Lotnisko docelowe")]
-        [Required]
         public virtual Airport Destination { get; set; }
 
         public virtual ICollection<Passenger> Passengers { get; set; }
 
         [Display(Name = "Data odlotu")]
         [Required]
-        public DateTime StartTime { get; set; }
+        public DateTime? StartTime { get; set; }
 
         [Display(Name = "Data przylotu")]
-        public DateTime FinishTime { get; set; }
+        public DateTime? FinishTime { get; set; }
     }
 }
